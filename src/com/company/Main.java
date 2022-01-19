da1e222515d6cc7ebe264d5e209a01614e11c417
IvanStoev1 <ivanparvi1@gmail.com>
2022-01-19 21:22:34 +0200

    Started writing the code for TTR

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static String text2 = scanner.nextLine();
 
     public static void main(String[] args) {
+        getTypeTokenRatio();
+//        System.out.println(getTypeTokenRatio());
         System.out.println("Average word length:" + getAverageWordLengthOfText1());
 
     }
@@ -16,28 +18,46 @@ public class Main {
     private static float getAverageWordLengthOfText1() {
         int count = 0;
         float sum = 0;
-        String[] words = getPunctuationStrippedText1(text1);
+        String[] words = getStrippedText1(text1);
         for (String word : words) {
             double wordLength = word.length();
             sum += wordLength;
             count++;
         }
 
-        float average = 0;
+        float averageWordLength = 0;
         if (count > 0) {
-            average = sum / count;
+            averageWordLength = sum / count;
         }
 
-        return average;
+        return averageWordLength;
 
     }
 
-    public static String[] getPunctuationStrippedText1(String text1) {
-        String strippedText1 = text1.replaceAll("\\p{Punct}", "");
-        String[] strippedSpaces = strippedText1.split(" ");
-        return strippedSpaces;
+    public static String[] getStrippedText1(String text1) {
+        String[] strippedText1 = text1.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+
+        return strippedText1;
 
     }
 
+    public static void getTypeTokenRatio() {
+        int allWords = 0;
+        String str = getPunctuationStrippedText1(text1);
+        String currentWord = null;
+        String[] words;
+        for (int i = 0; i < str.length(); i++) {
+            words = str.split(" ");
+            for (String word : words) {
+                currentWord = word;
+            }
+
+
+        }
+
+    }
+    public static String getPunctuationStrippedText1 (String text1){
+        return text1.replaceAll("\\p{Punct}", "");
+    }
+    
 }
-
